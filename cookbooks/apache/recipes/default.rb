1332bc195b66be87783de4491e7be7a75c5fcd3e
tomigit <contactme@sent.com>
2017-06-19 14:46:53 +0100

    added motd and changed apache def for node atr

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,13 +5,22 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 #Install Apache package:
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+
+elsif node ['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
-#Start nad enable Apache service:
+#Start and enable Apache service:
 service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,4 +1,4 @@
 file 'default www' do
 	path '/var/www/html/index.html'
-	content 'Hello wordl!'
+	content 'Hello world!'
 end
